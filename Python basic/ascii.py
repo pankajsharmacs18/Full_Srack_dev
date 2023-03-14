37bf73c90976b52c3c7fdf4d749f274af83f9c1c
Pankaj <pankajsharma9m@gmail.com>
2023-03-14 22:48:22 +0530

    Initial commit

---
+++ Python basic/ascii.py
@@ -0,0 +1,3 @@
+name = input("Ente string for find ascii value ")
+for i in name:
+    print("{0} : = : {1}".format(i,ord(i)))
---
+++ Python basic/lcm.py
@@ -0,0 +1,20 @@
+def lcmCal(n1, n2, larger):
+    if larger % n1 == 0 and larger % n2 == 0:
+        return larger
+    else:
+        print(larger)
+        lcmCal(n1, n2, larger + 1)
+
+
+num1 = int(input("Enter 1st number "))
+num2 = int(input("enter 2nd number "))
+lcm = 1
+if num1 % num2 == 0:
+    lcm = num1
+elif num2 % num1 == 0:
+    lcm = num2
+else:
+    large = num1 if num1 > num2 else num2
+    lcm = lcmCal(num1, num2, large)
+    print(lcm)
+print("LCM of {0} and {1} is {2} ".format(num1, num2, lcm))
